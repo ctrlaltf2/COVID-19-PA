eaf1e795bb21d80e35417ec20b612de810eb7013
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2020-04-16 23:52:47 -0400

    Add an easier-to-read PA total vs. new cases visualization

---
+++ pa_vis.m
@@ -0,0 +1,21 @@
+clear
+clc
+
+T = readtable('./positives.csv', 'Delimiter', ',');
+
+[r, c] = size(T);
+TotalCases = zeros(r, 1);
+
+for i=2:length(T.Properties.VariableNames)
+    TotalCases = TotalCases + T.(char(T.Properties.VariableNames(i)));
+end
+
+NewCases = diff(TotalCases);
+
+plot(TotalCases(2:end, 1), NewCases, '-')
+
+set(gca, 'xscale', 'log')
+set(gca, 'yscale', 'log')
+
+xlabel('Total Cases')
+ylabel('New Cases')
